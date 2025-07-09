e72885616bc1a0afce67376e7dbe2b600a56321b
Bảo <135942212+Drake-Vibe@users.noreply.github.com>
2025-07-09 09:15:37 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,7 @@ function App() {
                             <Route path="/detailRoute/*" element={<DetailRoute />} />
                             <Route path="/messages" element={<Messages />} />
                             <Route path="/chat" element={<ChatBox />} />
-                            <Route path="/payment" element={<Payment />} /> // Dùng 'Payment' cho tên component
+                            <Route path="/payment" element={<Payment />} />
                             <Route path="/staff/*" element={<StaffRoute />} />
                             {/* Thêm các route khác ở đây nếu có */}
                         </Routes>
